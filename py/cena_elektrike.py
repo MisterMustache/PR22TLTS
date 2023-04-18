817d2c6c8196030a74cfacbc294415d32d1a103c
duckduckwent <tadej.s02@gmail.com>
2023-04-18 21:19:15 +0200

    1 nov graf. Podatki o ceni elektrike

---
+++ py/cena_elektrike.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import csv
+
+with open('../podatki/csv/cena_elektrike.csv', encoding="cp1252") as f:
+    reader = csv.reader(f)
+    data = [row for row in reader]
+
+household = data[1]
+non_household = data[7]
+
+quarters = [x for x in data[0]][1:]
+colors = ['red', 'green', 'blue', 'yellow', 'cyan', 'magenta']
+
+plt.plot(quarters, [float(x) for x in household[1:]], label=household[0])
+plt.plot(quarters, [float(x) for x in non_household[1:]], label=non_household[0])
+plt.xlabel('Čas [četrtletje]')
+plt.ylabel('Cena [EUR/kWh]')
+plt.title('Cena električne energije')
+plt.xticks(rotation=-90)
+plt.legend(loc='best')
+plt.show()
